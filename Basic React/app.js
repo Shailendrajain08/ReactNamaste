2af69ce027f81e8b6c968f2d05467cafcef6a819
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-07-10 01:28:25 +0530

    Episode 3 complete

--- Basic React/app.js
+++ Basic React/app.js
@@ -1,25 +1,22 @@
-
-// <div>
-//     <div>
-//         <h1></h1>
-//     </div>
-// </div>
+import React from "react";
+import ReactDOM from "react-dom/client"
 
 
-const parent = React.createElement("div", {id : 'parent'}, [
-    React.createElement("div", {id : 'child1'}, [
-        React.createElement("h1", {id : 'h1'}, "I am a H1 inside child1 and parent div"),
-        React.createElement("h2", {id : 'h2'}, "I am a H2 inside child1 and parent div")
-    ]
-    ),
-    React.createElement("div", {id : 'child2'}, [
-        React.createElement("h1", {id : 'h1'}, "I am a H1 inside child2 and parent div"),
-        React.createElement("h2", {id : 'h2'}, "I am a H2 inside child2 and parent div")
-    ]
-    )
 
-]
-)
+// const parent = React.createElement("div", {id : 'parent'}, [
+//     React.createElement("div", {id : 'child1'}, [
+//         React.createElement("h1", {id : 'h1'}, "I am a H1 inside child1 and parent div Hello shailendra"),
+//         React.createElement("h2", {id : 'h2'}, "I am a H2 inside child1 and parent div")
+//     ]
+//     ),
+//     React.createElement("div", {id : 'child2'}, [
+//         React.createElement("h1", {id : 'h1'}, "I am a H1 inside child2 and parent div"),
+//         React.createElement("h2", {id : 'h2'}, "I am a H2 inside child2 and parent div")
+//     ]
+//     )
+
+// ]
+// )
 
 
 
@@ -28,8 +25,31 @@ const heading = React.createElement(
     { id: 'heading', xyz: "abc" },
     "Hello world form React!");
 
-    console.log(parent)
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)   
+console.log(heading)
 
-root.render(parent)
+const jsxHeading = <h1 id="heading">"Hello world form React using JSX!"</h1>
+
+console.log(jsxHeading)
+
+// React Functional Component 
+
+const Title = () => (
+    <h1>Title Component</h1>
+)
+
+const HeadingComponent = () => {
+    return (
+    <div className="container">
+        <Title/>
+        <h1>I am Inside React Functional Component</h1>
+    </div>
+    )
+    }
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>)
+
+
+
